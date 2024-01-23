931f1cd672e79520423a0c80fd8f10a18464b825
Fabian Jakobs <fabian.jakobs@databricks.com>
2024-01-23 16:36:07 +0100

    Inner dev loop fixes

--- default_python/tests/main_test.py
+++ default_python/tests/main_test.py
@@ -1,14 +1,15 @@
-from databricks.connect import DatabricksSession
-from pyspark.sql import SparkSession
+from databricks.connect import DatabricksSession as SparkSession
 from default_python import main
+from pytest import fixture
 
 # Create a new Databricks Connect session. If this fails,
 # check that you have configured Databricks Connect correctly.
 # See https://docs.databricks.com/dev-tools/databricks-connect.html.
 
-SparkSession.builder = DatabricksSession.builder
-SparkSession.builder.getOrCreate()
+@fixture(scope="session")
+def spark():
+    return SparkSession.builder.getOrCreate()
 
-def test_main():
-    taxis = main.get_taxis()
+def test_main(spark):
+    taxis = main.get_taxis(spark)
     assert taxis.count() > 5

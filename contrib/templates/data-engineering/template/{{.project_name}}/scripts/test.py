56cb31701789613aa241d89dda8630dc8594c169
Lennart Kats <lennart.kats@databricks.com>
2024-11-28 12:39:30 +0100

    Extend description

--- contrib/templates/data-engineering/template/{{.project_name}}/scripts/test.py
+++ contrib/templates/data-engineering/template/{{.project_name}}/scripts/test.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 #
-# test.py runs the unit tests for this project using pytest.
+# test.py runs the unit tests for this project using pytest and serverless compute.
+# To use a different form of compute, instead use 'uv run pytest' or
+# use your IDE's testing panel. When using VS Code, consider using the Databricks extension.
 #
 import os
 import subprocess

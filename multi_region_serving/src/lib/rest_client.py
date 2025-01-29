59ae338315c96e1be1e073c159f733146ceb115b
Mingyang Ge <mingyang.ge@databricks.com>
2025-01-29 13:11:12 -0800

    lint

--- multi_region_serving/src/lib/rest_client.py
+++ multi_region_serving/src/lib/rest_client.py
@@ -1,18 +1,21 @@
 import urllib.request
 import json
 from databricks.sdk.runtime import spark
- 
+
+
 class RestClient:
     def __init__(self, context):
         self.base_url = "https://" + spark.conf.get("spark.databricks.workspaceUrl")
         self.token = context.apiToken().get()
 
-    def get_share_info(self, share_name:str):
-        return self._get(f'api/2.1/unity-catalog/shares/{share_name}?include_shared_data=true')
-    
+    def get_share_info(self, share_name: str):
+        return self._get(
+            f"api/2.1/unity-catalog/shares/{share_name}?include_shared_data=true"
+        )
+
     def _get(self, uri):
-        url = f'{self.base_url}/{uri}'
-        headers = { 'Authorization': f'Bearer {self.token}'}
+        url = f"{self.base_url}/{uri}"
+        headers = {"Authorization": f"Bearer {self.token}"}
         req = urllib.request.Request(url, headers=headers)
         try:
             response = urllib.request.urlopen(req)

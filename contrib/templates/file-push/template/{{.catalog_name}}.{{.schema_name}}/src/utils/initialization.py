1301e007a23ff5d9c54cb88370e20f5e37c481f5
chi-yang-db <chi.yang@databricks.com>
2025-10-07 17:33:47 -0700

    Initial commit for file push

---
+++ contrib/templates/file-push/template/{{.catalog_name}}.{{.schema_name}}/src/utils/initialization.py
@@ -0,0 +1,69 @@
+from databricks.sdk import WorkspaceClient
+import argparse
+import json
+import logging
+
+# Parse arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("--catalog_name", type=str, required=True)
+parser.add_argument("--schema_name", type=str, required=True)
+parser.add_argument("--volume_path_root", type=str, required=True)
+parser.add_argument("--logging_level", type=str, required=False, default="dev")
+args = parser.parse_args()
+
+catalog_name = args.catalog_name
+schema_name = args.schema_name
+volume_path_root = args.volume_path_root
+volume_path_data = args.volume_path_root + "/data"
+volume_path_archive = args.volume_path_root + "/archive"
+logging_level = logging.DEBUG if args.logging_level == "dev" else logging.INFO
+
+# Logging
+logging.basicConfig(
+  level=logging_level,
+  format="%(asctime)s [%(levelname)s] %(module)s - %(message)s"
+)
+logger = logging.getLogger(__name__)  # per-module logger
+
+# Initialize workspace client
+ws = WorkspaceClient()
+
+# Set property to schema
+logger.info(f"Setting property to schema {catalog_name}.{schema_name}")
+logger.debug(f"Volume path root: {volume_path_root}")
+logger.debug(f"Volume path data: {volume_path_data}")
+ws.schemas.update(full_name=f"{catalog_name}.{schema_name}", properties={
+  "filepush.volume_path_root": volume_path_root,
+  "filepush.volume_path_data": volume_path_data,
+  "filepush.volume_path_data": volume_path_archive
+})
+logger.info(f"Schema {catalog_name}.{schema_name} configured")
+
+# Initialize volume folder structure
+logger.info(f"Initializing volume folder structure {volume_path_root}")
+logger.debug(f"Creating data directory {volume_path_data}")
+ws.files.create_directory(volume_path_data)
+logger.debug(f"Creating archive directory {volume_path_archive}")
+ws.files.create_directory(volume_path_archive)
+with open("../configs/tables.json", "r") as f:
+  for table in json.load(f):
+    table_volume_path_data = f"{volume_path_data}/{table['name']}"
+    logger.debug(f"Creating table directory {table_volume_path_data}")
+    ws.files.create_directory(table_volume_path_data)
+    table_volume_path_archive = f"{volume_path_archive}/{table['name']}"
+    logger.debug(f"Creating table archive directory {table_volume_path_archive}")
+    ws.files.create_directory(table_volume_path_archive)
+logger.info(f"Volume {volume_path_root} configured")
+
+# Dump configs to environment json
+all_configs = {
+  "catalog_name": catalog_name,
+  "schema_name": schema_name,
+  "volume_path_root": volume_path_root,
+  "volume_path_data": volume_path_data,
+  "volume_path_archive": volume_path_archive
+}
+with open("../configs/environment.json", "w") as f:
+  json.dump(all_configs, f)
+
+logger.info(f"==========\n%s\n==========", "\n".join(f"{k}: {v}" for k, v in all_configs.items()))
---
+++ contrib/templates/file-push/template/{{.catalog_name}}.{{.schema_name}}/src/utils/tablemanager.py
@@ -0,0 +1,98 @@
+import os
+import json
+from . import envmanager
+from . import formatmanager
+from pyspark.sql.streaming import DataStreamReader
+from pyspark.sql import DataFrame, SparkSession
+from databricks.sdk import WorkspaceClient
+from databricks.sdk.errors.platform import NotFound
+
+def validate_config(table_config: dict):
+  if not table_config.get("name"):
+    raise ValueError("name is required for table config")
+  if not table_config.get("format"):
+    raise ValueError("format is required for table config")
+
+def validate_configs(table_configs: list):
+  names = [cfg.get("name") for cfg in table_configs]
+  duplicates = set([name for name in names if names.count(name) > 1 and name is not None])
+  if duplicates:
+    raise ValueError(f"Duplicate table names found in table configs: {sorted(duplicates)}")
+  for table_config in table_configs:
+    validate_config(table_config)
+    
+def get_configs() -> list:
+  json_path = os.path.join(os.path.dirname(os.path.dirname(__file__)), "configs", "tables.json")
+  if not os.path.exists(json_path):
+    raise RuntimeError(f"Missing table configs file: {json_path}. Please following README.md to create one, deploy and run configuration_job.")
+  with open(json_path, "r") as f:
+    configs = json.load(f)
+  validate_configs(configs)
+  return configs
+
+def get_table_volume_path(table_name: str) -> str:
+  ws = WorkspaceClient()
+  table_volume_path_data = os.path.join(envmanager.get_config()["volume_path_data"], table_name)
+  try:
+    ws.files.get_directory_metadata(table_volume_path_data)
+  except NotFound:
+    raise RuntimeError(f"Table data path not found for table `{table_name}`. Have you run `databricks bundle run configuration_job`?")
+  return table_volume_path_data
+
+def has_data_file(table_name: str) -> bool:
+  ws = WorkspaceClient()
+  table_volume_path_data = get_table_volume_path(table_name)
+  try:
+    iter = ws.files.list_directory_contents(table_volume_path_data)
+    next(iter)
+  except StopIteration:
+    return False
+  return True
+
+def is_table_created(table_name: str) -> bool:
+  ws = WorkspaceClient()
+  return ws.tables.exists(full_name=f"{envmanager.get_config()['catalog_name']}.{envmanager.get_config()['schema_name']}.{table_name}").table_exists
+
+def _apply_table_options(reader: DataStreamReader, table_config: dict, fmt_mgr) -> DataStreamReader:
+  name = table_config.get("name")
+  fmt = table_config.get("format")
+
+  # format options
+  user_fmt_opts = table_config.get("format_options", {})
+  final_fmt_opts = fmt_mgr.get_merged_options(user_fmt_opts, name)
+  reader = reader.option("cloudFiles.format", fmt)
+  for k, v in final_fmt_opts.items():
+    reader = reader.option(k, v)
+  
+  # schema hints
+  schema_hints = table_config.get("schema_hints")
+  if schema_hints:
+    reader = reader.option("cloudFiles.schemaHints", ", ".join({schema_hints} | fmt_mgr.default_schema))
+  else:
+    reader = reader.option("cloudFiles.schemaHints", ", ".join(fmt_mgr.default_schema))
+
+  return reader
+
+def get_df_with_config(spark: SparkSession, table_config: dict, schema_location: str = None) -> DataFrame:
+  validate_config(table_config)
+  fmt = table_config.get("format")
+  fmt_mgr = formatmanager.get_format_manager(fmt)
+
+  reader = spark.readStream.format("cloudFiles")
+  reader = _apply_table_options(reader, table_config, fmt_mgr)
+  if schema_location:
+    reader = reader.option("cloudFiles.schemaLocation", schema_location)
+
+  # include file metadata
+  return reader.load(get_table_volume_path(table_config.get("name"))).selectExpr("*", "_metadata")
+
+def get_placeholder_df_with_config(spark: SparkSession, table_config: dict) -> DataFrame:
+  validate_config(table_config)
+  fmt = table_config.get("format")
+  fmt_mgr = formatmanager.get_format_manager(fmt)
+
+  reader = spark.readStream.format("cloudFiles")
+  reader = _apply_table_options(reader, table_config, fmt_mgr).schema(fmt_mgr.get_default_schema())
+
+  return reader.load(get_table_volume_path(table_config.get("name")))
+  

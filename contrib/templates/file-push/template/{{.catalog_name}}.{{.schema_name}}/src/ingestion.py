cf6834b174ad4188449ecab90a74ca02666148a2
chi-yang-db <chi.yang@databricks.com>
2025-10-17 16:50:39 -0700

    Ran ruff formatter

--- contrib/templates/file-push/template/{{.catalog_name}}.{{.schema_name}}/src/ingestion.py
+++ contrib/templates/file-push/template/{{.catalog_name}}.{{.schema_name}}/src/ingestion.py
@@ -2,34 +2,36 @@ import dlt
 from utils import tablemanager
 from utils import formatmanager
 
+
 def _make_append_flow(table_name, table_config, table_volume_path):
-  def _body():
-    # use _rescued_data as placeholder when no data file is present
-    if not tablemanager.has_data_file(table_name):
-      return tablemanager.get_placeholder_df_with_config(spark, table_config)
-    else:
-      return tablemanager.get_df_with_config(spark, table_config)
+    def _body():
+        # use _rescued_data as placeholder when no data file is present
+        if not tablemanager.has_data_file(table_name):
+            return tablemanager.get_placeholder_df_with_config(spark, table_config)
+        else:
+            return tablemanager.get_df_with_config(spark, table_config)
 
-  # give the function a unique name
-  _body.__name__ = f"append_{table_name.lower()}"
+    # give the function a unique name
+    _body.__name__ = f"append_{table_name.lower()}"
 
-  # apply the decorator programmatically
-  dlt.append_flow(target=table_name, name=table_name)(_body)
+    # apply the decorator programmatically
+    dlt.append_flow(target=table_name, name=table_name)(_body)
+
 
 table_configs = tablemanager.get_configs()
 
 # create the tables and append flows
 for cfg in table_configs:
-  tablemanager.validate_config(cfg)
-  tbl = cfg["name"]
-  path = tablemanager.get_table_volume_path(tbl)
-  fmt = formatmanager.get_format_manager(cfg["format"])
-  expts = fmt.expectations
+    tablemanager.validate_config(cfg)
+    tbl = cfg["name"]
+    path = tablemanager.get_table_volume_path(tbl)
+    fmt = formatmanager.get_format_manager(cfg["format"])
+    expts = fmt.expectations
 
-  dlt.create_streaming_table(
-    name=tbl,
-    comment="File push created table",
-    table_properties={"filepush.table_volume_path_data": path},
-    expect_all=expts
-  )
-  _make_append_flow(tbl, cfg, path)
+    dlt.create_streaming_table(
+        name=tbl,
+        comment="File push created table",
+        table_properties={"filepush.table_volume_path_data": path},
+        expect_all=expts,
+    )
+    _make_append_flow(tbl, cfg, path)
--- contrib/templates/file-push/template/{{.catalog_name}}.{{.schema_name}}/src/utils/initialization.py
+++ contrib/templates/file-push/template/{{.catalog_name}}.{{.schema_name}}/src/utils/initialization.py
@@ -20,8 +20,7 @@ logging_level = logging.DEBUG if args.logging_level == "dev" else logging.INFO
 
 # Logging
 logging.basicConfig(
-  level=logging_level,
-  format="%(asctime)s [%(levelname)s] %(module)s - %(message)s"
+    level=logging_level, format="%(asctime)s [%(levelname)s] %(module)s - %(message)s"
 )
 logger = logging.getLogger(__name__)  # per-module logger
 
@@ -32,11 +31,14 @@ ws = WorkspaceClient()
 logger.info(f"Setting property to schema {catalog_name}.{schema_name}")
 logger.debug(f"Volume path root: {volume_path_root}")
 logger.debug(f"Volume path data: {volume_path_data}")
-ws.schemas.update(full_name=f"{catalog_name}.{schema_name}", properties={
-  "filepush.volume_path_root": volume_path_root,
-  "filepush.volume_path_data": volume_path_data,
-  "filepush.volume_path_data": volume_path_archive
-})
+ws.schemas.update(
+    full_name=f"{catalog_name}.{schema_name}",
+    properties={
+        "filepush.volume_path_root": volume_path_root,
+        "filepush.volume_path_data": volume_path_data,
+        "filepush.volume_path_data": volume_path_archive,
+    },
+)
 logger.info(f"Schema {catalog_name}.{schema_name} configured")
 
 # Initialize volume folder structure
@@ -46,24 +48,27 @@ ws.files.create_directory(volume_path_data)
 logger.debug(f"Creating archive directory {volume_path_archive}")
 ws.files.create_directory(volume_path_archive)
 with open("../configs/tables.json", "r") as f:
-  for table in json.load(f):
-    table_volume_path_data = f"{volume_path_data}/{table['name']}"
-    logger.debug(f"Creating table directory {table_volume_path_data}")
-    ws.files.create_directory(table_volume_path_data)
-    table_volume_path_archive = f"{volume_path_archive}/{table['name']}"
-    logger.debug(f"Creating table archive directory {table_volume_path_archive}")
-    ws.files.create_directory(table_volume_path_archive)
+    for table in json.load(f):
+        table_volume_path_data = f"{volume_path_data}/{table['name']}"
+        logger.debug(f"Creating table directory {table_volume_path_data}")
+        ws.files.create_directory(table_volume_path_data)
+        table_volume_path_archive = f"{volume_path_archive}/{table['name']}"
+        logger.debug(f"Creating table archive directory {table_volume_path_archive}")
+        ws.files.create_directory(table_volume_path_archive)
 logger.info(f"Volume {volume_path_root} configured")
 
 # Dump configs to environment json
 all_configs = {
-  "catalog_name": catalog_name,
-  "schema_name": schema_name,
-  "volume_path_root": volume_path_root,
-  "volume_path_data": volume_path_data,
-  "volume_path_archive": volume_path_archive
+    "catalog_name": catalog_name,
+    "schema_name": schema_name,
+    "volume_path_root": volume_path_root,
+    "volume_path_data": volume_path_data,
+    "volume_path_archive": volume_path_archive,
 }
 with open("../configs/environment.json", "w") as f:
-  json.dump(all_configs, f)
+    json.dump(all_configs, f)
 
-logger.info(f"==========\n%s\n==========", "\n".join(f"{k}: {v}" for k, v in all_configs.items()))
+logger.info(
+    f"==========\n%s\n==========",
+    "\n".join(f"{k}: {v}" for k, v in all_configs.items()),
+)

66f77200b1557196f852524a682eb77312e0554f
pegodk <pegodk>
2025-11-19 18:01:30 +0000

    Cleanup

--- src/fake_lakeflow_pipeline/transformations/ingest_fake_data.py
+++ src/fake_lakeflow_pipeline/transformations/ingest_fake_data.py
@@ -11,48 +11,7 @@ from framework.utils import add_metadata_columns
 
 # Configuration
 config = Config.from_spark_config()
-
-# # Custom Faker data source implementation
-# class FakeDataSourceReader(DataSourceReader):
-
-#     def __init__(self, schema, options):
-#         self.schema: StructType = schema
-#         self.options = options
-
-#     def read(self, partition):
-#         # Library imports must be within the method.
-#         from faker import Faker
-#         fake = Faker()
-
-#         # Every value in this `self.options` dictionary is a string.
-#         num_rows = int(self.options.get("numRows", 3))
-#         for _ in range(num_rows):
-#             row = []
-#             for field in self.schema.fields:
-#                 value = getattr(fake, field.name)()
-#                 row.append(value)
-#             yield tuple(row)
-
-
-# class FakeDataSource(DataSource):
-#     """
-#     An example data source for batch query using the `faker` library.
-#     """
-
-#     @classmethod
-#     def name(cls):
-#         return "fake"
-
-#     def schema(self):
-#         return "name string, date string, zipcode string, state string"
-
-#     def reader(self, schema: StructType):
-#         return FakeDataSourceReader(schema, self.options)
-
-
-# # Register the data source
-# spark.dataSource.register(FakeDataSource)
-
+spark = SparkSession.getActiveSession()
 
 # Table definition
 @dlt.table(

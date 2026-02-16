19a9ef57d7e93df99d1ae5797c58488b96b04e12
Zanita Rahimi <zanita.rahimi06@gmail.com>
2026-02-16 11:42:03 +0100

    update the table update trigger example

--- knowledge_base/pydabs_job_table_update_trigger/resources/table_update.py
+++ knowledge_base/pydabs_job_table_update_trigger/resources/table_update.py
@@ -1,16 +1,18 @@
-from databricks.bundles.jobs import Job, NotebookTask, Task, TableUpdateTriggerConfiguration
+from databricks.bundles.jobs import Job, NotebookTask, Task, TriggerSettings, TableUpdateTriggerConfiguration
 
 consume_table = Task(
     task_key="consume_table",
-    notebook_task=NotebookTask(notebook_path="src/assets/consume_table.py"),
+    notebook_task=NotebookTask(notebook_path="src/assets/consume_table.ipynb"),
 )
 
 job = Job(
     name="table_update_example",
-    trigger=TableUpdateTriggerConfiguration(
+    trigger=TriggerSettings(
+        table_update=TableUpdateTriggerConfiguration(
         table_names=["main.analytics.daily_events"],
         min_time_between_triggers_seconds=0,
         wait_after_last_change_seconds=3600,
+        )
     ),
     tasks=[consume_table],
 )
--- knowledge_base/pydabs_job_table_update_trigger/src/assets/consume_table.py
+++
@@ -1,5 +0,0 @@
-from pyspark.sql import functions as F
-
-source_table = "main.analytics.daily_events"
-# Insert consumer logic here
-df = spark.read.table(source_table)

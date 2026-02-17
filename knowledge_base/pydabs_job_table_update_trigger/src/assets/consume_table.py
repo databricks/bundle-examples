c2bfa57ee026f4193397f554e244830689ed03ef
Lorenzo Rubio <lorenzo.rubio@databricks.com>
2026-02-17 15:27:46 +0100

    final review

--- knowledge_base/pydabs_job_table_update_trigger/src/assets/consume_table.py
+++ knowledge_base/pydabs_job_table_update_trigger/src/assets/consume_table.py
@@ -1,6 +1,4 @@
 # Databricks notebook source
-from pyspark.sql import functions as F
-
 source_table = "main.analytics.daily_events"
 # Insert consumer logic here
 df = spark.read.table(source_table)
